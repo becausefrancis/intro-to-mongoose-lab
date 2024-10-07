d2329c6ac1df246f8e74a83afa2c8a6fb99feaa2
becausefrancis <jimenez.francis92@gmail.com>
2024-10-06 21:43:03 -0500

    Built foundation code.

---
+++ app.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+
+// const username = prompt('What is your name? ');
+// console.log(`Your name is ${username}`);
+
+console.log('Welcome to the CRM');
+
+const action = prompt('What would you like to do? ');
